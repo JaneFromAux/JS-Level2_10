c55a33e11adad13e746f399e8006dc7a0a6eb11d
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-10 14:51:04 +0200

    fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
+let text = document.getElementById('text');
 
 
 
 
 function small() {
     console.log('small');
+    text.classList.remove('middle', 'big');
+    text.classList.add('small');
+
 }
 function middle() {
     console.log('middle');
+    text.classList.remove('small', 'big');
+    text.classList.add('middle');
 }
 function big() {
     console.log('big');
+    text.classList.remove('small', 'middle');
+    text.classList.add('big');
 }
